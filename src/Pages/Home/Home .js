c52083379a6a15886a4728ba1f6498a10ae66629
Naiemisalm <nayemislam30964@gmail.com>
2022-05-11 10:24:26 +0600

    added postal

--- src/Pages/Home/Home .js
+++ src/Pages/Home/Home .js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
 import Info from './Info';
+import Postal from './Postal';
 import Services from './Services';
 
 const Home  = () => {
@@ -9,6 +10,7 @@ const Home  = () => {
             <Banner></Banner>
             <Info></Info>
             <Services></Services>
+            <Postal></Postal>
         </div>
     );
 };
