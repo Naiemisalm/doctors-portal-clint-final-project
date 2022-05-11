3de4daf68b27f67c867ab55ad6b7d1e7ac845e0e
Naiemisalm <nayemislam30964@gmail.com>
2022-05-11 11:10:30 +0600

    Doctors Appointment

---
+++ src/Pages/Home/MakeAppointment.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import doctors from '../../assets/images/doctor.png'
+import appointment from '../../assets/images/appointment.png'
+import Button from '../Shared/Button';
+
+
+const MakeAppointment = () => {
+    return (
+        <section style={{
+            background: `url(${appointment})`
+        }}
+        
+        className='flex justify-center items-center'>
+            <div className='flex-1 hidden lg:block'>
+                <img className='mt-[-150px]' src={doctors} alt='' />
+            </div>
+            <div className='flex-1'>
+                <h2 className='text-l text-primary text-bold'>Appointment</h2>
+                <h3 className='text-4xl text-white'>Make an appointment Today</h3>
+                <p className='text-white'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                <Button> Get Started</Button>
+            </div>
+        </section>
+    );
+};
+
+export default MakeAppointment;
--- src/Pages/Home/Postal.js
+++ src/Pages/Home/Postal.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import treatment from '../../assets/images/treatment.png'
+import Button from '../Shared/Button';
 
 const Postal = () => {
     return (
         <div>
-            <div class="hero min-h-screen">
+            <div class="hero min-h-screen mb-12">
                 <div class="hero-content flex-col lg:flex-row">
                     <img src={treatment} class="max-w-sm rounded-lg shadow-2xl" />
                     <div className='px-8'>
                         <h1 class="text-5xl font-bold">Exceptional Dental <br></br> Care, on Your Terms</h1>
                         <p class="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
-                        <button class="btn btn-primary bg-gradient-to-r from-secondary to-primary">Get Started</button>
+                        <Button>Get Started</Button>
                     </div>
                 </div>
             </div>
