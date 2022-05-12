d15b4a338cad63083cc4d3cefec6546bdcca8144
Naiemisalm <nayemislam30964@gmail.com>
2022-05-12 18:02:54 +0600

    firebase inisilaize

--- src/Pages/Appointment/AvalableAppointment.js
+++ src/Pages/Appointment/AvalableAppointment.js
@@ -3,27 +3,31 @@ import React, { useEffect, useState } from 'react';
 import BookingModal from './BookingModal';
 import Service from './Service';
 
-const AvalableAppointment = ({date}) => {
+const AvalableAppointment = ({ date }) => {
     const [services, setServices] = useState([]);
     const [tritment, setTritment] = useState(null)
-    useEffect( () =>{
-        fetch('Services.json')
-        .then(res=>res.json())
-        .then(data => setServices(data))
-    },[])
+    useEffect(() => {
+        fetch('http://localhost:5000/service')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
     return (
         <div>
-           <h3 className='text-center text-xl text-secondary'> Available Appointments on  {format(date,'pp')}</h3>
-           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-               {
-                   services.map(service=> <Service
-                   key={service._id}
-                   service={service}
-                   setTritment ={setTritment}
-                   ></Service>)
-               }
-           </div>
-           {tritment && <BookingModal tritment={tritment}></BookingModal>}
+            <h3 className='text-center text-xl text-secondary mb-12'> Available Appointments on  {format(date, 'pp')}</h3>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}
+                        setTritment={setTritment}
+                    ></Service>)
+                }
+            </div>
+            {tritment && <BookingModal
+                date={date}
+                tritment={tritment}
+                setTritment={setTritment}
+            ></BookingModal>}
         </div>
     );
 };
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -4,8 +4,8 @@ const Service = ({ service, setTritment }) => {
     const {name, slots} = service;
     return (
         <div class="card w-96 bg-base-100 shadow-xl">
-            <div class="card-body">
-                <h2 class="card-title text-secondary">{name}</h2>
+            <div class="card-body text-center">
+                <h2 class="text-xl font-bold text-secondary">{name}</h2>
                 <p>
                     {
                         slots.length > 0
@@ -21,7 +21,7 @@ const Service = ({ service, setTritment }) => {
                       for="booking-modal"
                       disabled={slots.length === 0}
                       onClick={()=>{setTritment(service)}}
-                     class="btn btn-secondary text-white uppercase ">Book Appointment</label>
+                     class="btn btn-sm btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary ">Book Appointment</label>
                     
                 </div>
             </div>
